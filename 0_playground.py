d1666d44226274f13af25cf878cd63a24e1c5528
Rex Zhang <rex.zhang@compass.com>
2021-08-04 00:01:07 -0700

    2021

--- 0_playground.py
+++ 0_playground.py
@@ -1,34 +1,86 @@
-class Solution(object):
-    def divide(self, dividend, divisor):
-        INT_MAX = 2147483647
-        if divisor == 0:
-            return INT_MAX
-        neg = dividend > 0 and divisor < 0 or dividend < 0 and divisor > 0
-        a, b = abs(dividend), abs(divisor)
-        ans, shift = 0, 31
-        while shift >= 0:
-            print(shift, a, b, ans)
-            if a >= b << shift:
-                a -= b << shift
-                ans += 1 << shift
-            shift -= 1
-        if neg:
-            ans = - ans
-        if ans > INT_MAX:
-            return INT_MAX
-        return ans
+# class Solution(object):
+#     def divide(self, dividend, divisor):
+#         INT_MAX = 2147483647
+#         if divisor == 0:
+#             return INT_MAX
+#         neg = dividend > 0 and divisor < 0 or dividend < 0 and divisor > 0
+#         a, b = abs(dividend), abs(divisor)
+#         ans, shift = 0, 31
+#         while shift >= 0:
+#             print(shift, a, b, ans)
+#             if a >= b << shift:
+#                 a -= b << shift
+#                 ans += 1 << shift
+#             shift -= 1
+#         if neg:
+#             ans = - ans
+#         if ans > INT_MAX:
+#             return INT_MAX
+#         return ans
 
 
-import math
-def area(radius):
-    return math.pi * (radius ** 2)
+# import math
+# def area(radius):
+#     return math.pi * (radius ** 2)
 
-l = [1,2,3,4,5,6]
-x = map(area, l)
+# l = [1,2,3,4,5,6]
+# x = map(area, l)
 
-print(x)
+# print(x)
 
 
-rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
-q = [(i, j) for i, row in enumerate(rooms) for j, r in enumerate(row) if not r]
-print(q)
+# rooms = [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
+# q = [(i, j) for i, row in enumerate(rooms) for j, r in enumerate(row) if not r]
+# print(q)
+
+
+
+"""
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+"""
+
+
+
+
+def sum_three(nums):
+    results = []
+    list.sort(nums)
+    for x in range(len(nums)-2):
+        head_ptr = x + 1
+        tail_ptr = len(nums) - 1
+        curr_sum = nums[x] + nums[head_ptr] + nums[tail_ptr]
+        while curr_sum != 0:
+            while nums[head_ptr - 1] == nums[head_ptr]:
+                head_ptr += 1
+            while nums[tail_ptr -1] == nums[tail_ptr]:
+                tail_ptr -= 1
+            if head_ptr >= tail_ptr:
+                break
+            if curr_sum > 0:
+                tail_ptr -= 1
+            else:
+                head_ptr += 1    
+            curr_sum = nums[x] + nums[head_ptr] + nums[tail_ptr]  
+        if curr_sum == 0:
+            results.append([nums[x], nums[head_ptr], nums[tail_ptr]])
+    return results
+    
+    
+    
+t1 = [-1,0,1,2,-1,-4]
+
+print(sum_three(t1))
--- BFS/Examples/GraphValidTree.py
+++ BFS/Examples/GraphValidTree.py
@@ -5,7 +5,6 @@ Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge 
 
 You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
 
-Have you met this question in a real interview? Yes
 Example
 Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
 
@@ -20,31 +19,31 @@ from collections import defaultdict, deque
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
+
         # 一个图是树需要满足两个条件：
         # 1. N个点只能有N-1条边
         # 2. N 个点互相连通
-        
+
         if n -1 != len(edges):
             return False
-        
+
         # 用字典表示关系
         neighbors = defaultdict(list)
         for u, v in edges:
             neighbors[u].append(v)
             neighbors[v].append(u)
-        
+
         visited = set()
         visited.add(0)
         q = deque([0])
-        
+
         while q:
             node = q.popleft()
-            
+
             for neighbor in neighbors[node]:
                 if neighbor not in visited:
                     q.append(neighbor)
                     visited.add(neighbor)
-        
+
 
         return len(visited) == n
